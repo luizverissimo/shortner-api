0bec58d5da9f884959a263e395f77cf41d24691e
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-06 19:49:02 -0300

    changing variable name to more legibility of code

--- src/repositories/urls-repository.ts
+++ src/repositories/urls-repository.ts
@@ -2,5 +2,5 @@ import { Prisma, Url } from '@prisma/client'
 
 export interface UrlsRepository {
   create(data: Prisma.UrlCreateInput): Promise<Url>
-  findByAlias(q: string): Promise<Url | null>
+  findByAlias(alias: string): Promise<Url | null>
 }
