a5decb8df09e9c89b59ab3d46ed9f8746abe7e32
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-06 19:49:36 -0300

    implementing abstract repository

--- src/repositories/prisma/prisma-url-repository.ts
+++ src/repositories/prisma/prisma-url-repository.ts
@@ -1,10 +1,16 @@
-import { Prisma } from '@prisma/client'
+import { Prisma, Url } from '@prisma/client'
 import { UrlsRepository } from '../urls-repository'
 import { prisma } from '../../../prisma/lib/prisma'
 
 export class PrismaUrlRepository implements UrlsRepository {
-  async create(data: Prisma.URLCreateInput): Promise<URL> {
-    const url = await prisma.uRL.create({ data })
+  async findByAlias(alias: string): Promise<Url | null> {
+    const url = await prisma.url.findFirst({ where: { alias } })
+
+    return url
+  }
+
+  async create(data: Prisma.UrlCreateInput): Promise<Url> {
+    const url = await prisma.url.create({ data })
 
     return url
   }
