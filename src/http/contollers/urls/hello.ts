f8fbdb2e1f97d8efbf7d1cf378a295d11d45bb03
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-06 19:45:42 -0300

    removing url test

--- src/http/contollers/urls/hello.ts
+++
@@ -1,5 +0,0 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
-
-export async function hello(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send({ hello: 'world' })
-}
