abe57828902fb959483c90768529a59b17af869c
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-06 19:46:18 -0300

    creating url controller

--- src/http/contollers/urls/create.ts
+++ src/http/contollers/urls/create.ts
@@ -1,5 +1,19 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeCreateUrlUseCase } from '../../../use-cases/factories/make-create-url-use-case'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send({ hello: 'world' })
+  const createUrlBodySchema = z.object({
+    origin: z.string(),
+    alias: z.string(),
+  })
+  const { origin, alias } = createUrlBodySchema.parse(request.body)
+
+  const createUrlUseCase = makeCreateUrlUseCase()
+  await createUrlUseCase.execute({
+    origin,
+    alias,
+  })
+
+  return reply.status(201).send()
 }
