8535de6fa3c739c9f66820b46036458215a621a8
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-06 23:40:38 -0300

    adding error console log

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import Fastify from 'fastify'
 import { urlsRoutes } from './http/contollers/urls/routes'
 import { ZodError } from 'zod'
+import { env } from './env'
 
-export const app = Fastify({ logger: true })
+export const app = Fastify()
 app.register(urlsRoutes)
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -10,5 +11,12 @@ app.setErrorHandler((error, _, reply) => {
       .status(400)
       .send({ message: 'Validation Error', issues: error.format() })
   }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO here should log to an external tool like Kibana/new Relic
+  }
+
   return reply.status(500).send({ message: 'Internal Server Error' })
 })
