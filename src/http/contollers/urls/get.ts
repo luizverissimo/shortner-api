27e3c2a7ce3dafb32ecc0ee6c2e18ac4e3d6b942
Luiz Verissimo <luizverissimosouza@gmail.com>
2024-03-07 01:04:59 -0300

    add error catch in get controller

--- src/http/contollers/urls/get.ts
+++ src/http/contollers/urls/get.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { makeCreateAccessUseCase } from '../../../use-cases/factories/make-create-access-use-case'
 import { makeGetUrlFromAliasUseCase } from '../../../use-cases/factories/make-get-url-from-alias'
+import { AliasIsNotFoundError } from '../../../use-cases/errors/alias-is-not-found'
 
 export async function get(request: FastifyRequest, reply: FastifyReply) {
   const getUrlBodySchema = z.object({
@@ -9,15 +10,23 @@ export async function get(request: FastifyRequest, reply: FastifyReply) {
   })
   const { alias } = getUrlBodySchema.parse(request.params)
 
-  const getUlrFromAliasUseCase = makeGetUrlFromAliasUseCase()
-  const { url } = await getUlrFromAliasUseCase.execute({
-    alias,
-  })
+  try {
+    const getUlrFromAliasUseCase = makeGetUrlFromAliasUseCase()
+    const { url } = await getUlrFromAliasUseCase.execute({
+      alias,
+    })
 
-  const createAccessUseCase = makeCreateAccessUseCase()
-  await createAccessUseCase.execute({
-    urlId: url.id,
-  })
+    const createAccessUseCase = makeCreateAccessUseCase()
+    await createAccessUseCase.execute({
+      urlId: url.id,
+    })
+  } catch (err) {
+    if (err instanceof AliasIsNotFoundError) {
+      return reply.status(404).send({ message: err.message })
+    }
+
+    throw err // passing responsibility to fastify
+  }
 
   return reply.status(200).send({ url: url.origin })
 }
